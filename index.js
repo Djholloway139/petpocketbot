eed6ad2dbaa77e57805772b1b3eb81bac059eb4f
Djholloway139 <dan@scrapthe9to5.com>
2025-07-08 18:08:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,11 @@ app.post('/webhook', async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
+  headless: true,
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
+  executablePath: puppeteer.executablePath()
+});
+
 
     const page = await browser.newPage();
     await page.goto('https://www.petpocketbook.com/login', { waitUntil: 'networkidle2' });
